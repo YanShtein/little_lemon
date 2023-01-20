6ac09d0fcc627f93c6c0742e7bee893a48ef174e
Yan Shtein <85824489+YanShtein@users.noreply.github.com>
2023-01-20 09:29:28 +0200

    fixed article image

--- src/components/Home/Article.js
+++ src/components/Home/Article.js
@@ -3,15 +3,18 @@ import { Link } from "react-router-dom";
 export default function Article() {
   return (
     <div className="article">
+      <img src={require('../../assets/images/main.JPG')} alt='Menu'/>
       <div className="article-content">
         <h2>Little Lemon</h2>
         <h3>Chicago</h3>
-        <p>We are a family mediterranean restaurant.
-          focused on traditional recipes, served with a modern twist.
+        <p title="Thanks to chatGPT for generating the Little Lemon about text.">
+          Little Lemon is a charming restaurant located in the heart of downtown.<br />
+          The menu features a mix of classic and contemporary dishes made with fresh, locally-sourced ingredients.<br />
+          Whether you're in the mood for a casual lunch or a romantic dinner,<br />
+          Little Lemon has something to satisfy your cravings.
         </p>
         <Link to="/book"><button>Book a table</button></Link>
       </div>
-      <img src={require('../../assets/images/main.JPG')} alt='Menu'/>
     </div>
   )
 };
