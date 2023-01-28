65fe3d60a4550591bcacfa1b335196c9874dc9a5
Yan Shtein <85824489+YanShtein@users.noreply.github.com>
2023-01-28 11:54:31 +0200

    finished project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,9 +18,19 @@ export default function Header() {
         <div className="hamburger-content">
           <span
             className="hamburger-content-exit"
-            onClick={() => setOpen(false)}>{exitSvg}
+            onClick={() => setOpen(false)}
+            >
+            {exitSvg}
           </span>
-          <Nav />
+          <nav>
+            <ul>
+              <li><Link to="/" aria-label="Go to Home Page" onClick={() => setOpen(false)}>Home</Link></li>
+              <li><Link to="/#about" aria-label="Go to About Section" onClick={() => setOpen(false)}>About</Link></li>
+              <li><Link to="/menu" aria-label="Go to Menu Page" onClick={() => setOpen(false)}>Menu</Link></li>
+              <li><Link to="/book" aria-label="Go to Book a table Page" onClick={() => setOpen(false)}>Book a Table</Link></li>
+              <li><Link to="/order" aria-label="Go to Order Online Page" onClick={() => setOpen(false)}>Order Online</Link></li>
+            </ul>
+          </nav>
         </div>
       </div>
       <div className="header fixed">
